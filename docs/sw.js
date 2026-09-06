66707b54f98e17a577081f7c3562c69ca79aaa33
Paulo <pamangiapane@gmail.com>
2026-09-06 17:53:36 +0200

    feat(ui): replica estetica vecchie schede, calcolo % 1RM con dettaglio manubri e CMJ 1x3 al centro seduta

--- docs/sw.js
+++ docs/sw.js
@@ -1,12 +1,12 @@
-// Service Worker App Marsala Volley (v208-clean)
-const CACHE_NAME = 'marsala-pwa-v208-clean';
+// Service Worker App Marsala Volley (v209-aesthetic)
+const CACHE_NAME = 'marsala-pwa-v209-aesthetic';
 
 
 
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './data.js?v=20260906week34',
+  './data.js?v=20260906aesthetic',
   './logo.jpg',
   './icon-192.png',
   './icon-512.png',
